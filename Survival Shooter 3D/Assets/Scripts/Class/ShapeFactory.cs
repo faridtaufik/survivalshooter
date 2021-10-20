c4821a8e6606ac37908801f0e67ef42f84ae1b16
Farid Taufik <90214234+faridtaufik@users.noreply.github.com>
2021-10-20 16:57:42 +0700

    survivalshooter

---
+++ Survival Shooter 3D/Assets/Scripts/Class/ShapeFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeFactory 
+{
+   //use getShape method to get object of type shape 
+   public Shape getShape(string shapeType){
+      if(shapeType == null){
+         return null;
+      }		
+      if(shapeType.Equals("CIRCLE")){
+         return new Circle();
+         
+      } else if(shapeType.Equals("RECTANGLE")){
+         return new Rectangle();
+         
+      } else if(shapeType.Equals("SQUARE")){
+         return new Square();
+      }
+      
+      return null;
+   }
+}
---
+++ Survival Shooter 3D/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    public bool IsOver { get; private set; }
+
+    [SerializeField]
+    MonoBehaviour factory;
+    IFactory Factory { get { return factory as IFactory; } }
+
+    void Start()
+    {
+        //Mengeksekusi fungs Spawn setiap beberapa detik sesui dengan nilai spawnTime
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+    void Spawn()
+    {
+        //Jika player telah mati maka tidak membuat enemy baru
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        //Mendapatkan nilai random
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int spawnEnemy = Random.Range(0, 3);
+
+        //Memduplikasi enemy
+        Factory.FactoryMethod(spawnEnemy);
+    }
+    private void Update()
+    {
+        // Jika menekan tombol R, fungsi restart akan terpanggil
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (IsOver)
+        {
+            return;
+        }
+    }
+    }
